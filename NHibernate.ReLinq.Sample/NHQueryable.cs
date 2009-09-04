6f9f35b3065283b1bb04b8079300ab929c201b8e
Markus Giegl <markus.giegl@rubicon.eu>
2009-09-04 12:26:43 +0000

    NHibernate.ReLinq.Sample - changed license headers to MIT, updated readme, deleted lgp txt

--- NHibernate.ReLinq.Sample/NHQueryable.cs
+++ NHibernate.ReLinq.Sample/NHQueryable.cs
@@ -6,16 +6,8 @@
 //  NHibernate.ReLinq.Sample is based on re-motion re-linq (http://www.re-motion.org/).
 //  
 //  NHibernate.ReLinq.Sample is free software; you can redistribute it 
-//  and/or modify it under the terms of the GNU Lesser General Public License 
-//  version 3.0 as published by the Free Software Foundation.
-//  
-//  re-motion is distributed in the hope that it will be useful, 
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of 
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-//  GNU Lesser General Public License for more details.
-//  
-//  You should have received a copy of the GNU Lesser General Public License
-//  along with NHibernate.ReLinq.Sample; if not, see http://www.gnu.org/licenses.
+//  and/or modify it under the terms of the MIT License 
+// (http://www.opensource.org/licenses/mit-license.php).
 // 
 using System;
 using System.Linq;
